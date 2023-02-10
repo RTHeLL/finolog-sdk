f52ad2587e0b3317187f1cb6cb0a40f0f1f5a415
RTHeLL <k1ndermail@ya.ru>
2023-02-10 18:13:42 +0300

    fix pypi

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+value=`cat finolog/_version.py  | cut -d '"' -f 2`
+echo "$value"
+git tag "v$value"
+git push origin --tags
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload --skip-existing --repository pypi dist/*
